928de94aec02ae037af5c8000960969dfdd53121
brooke <9060826518ng@gmail.com>
2020-11-30 14:18:19 +0300

    Add Blog model and connect Postgre

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title_blog = models.CharField(max_length=30)
+    date_blog = models.DateTimeField()
+    text_blog = models.CharField(max_length=300)
+    image_blog = models.ImageField(upload_to='event_images/')
--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'events.apps.EventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'myawesomeblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myawesomeblogdb',
+        'USER': 'postgres',
+        'PASSWORD': 'nikita2002postgre',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
